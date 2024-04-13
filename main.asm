46a0689273d8ac080a6acd54b394263e98bb0fd9
Zanderp25 <zander@zanderp25.com>
2024-04-13 14:28:37 -0400

    Create Microchip Studio project files

---
+++ main.asm
@@ -0,0 +1,12 @@
+;
+; stoply.asm
+;
+; Created: 2024-04-13 14:15:16
+; Author : Zander Picon, Jordan Robertson, Daniel De Sousa
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
